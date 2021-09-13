fb4aef3bc83f2b3b2d2d879397a910bf3737842d
Srini Bala <37417818+nivasbala@users.noreply.github.com>
2021-09-13 14:14:29 -0700

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 	//	f := fib()
 
-		res := &response{Message: "Hello World - Version 1"}
+		res := &response{Message: "Hello World - Version 222222"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
